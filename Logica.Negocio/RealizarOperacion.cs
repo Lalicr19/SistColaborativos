f7d89d97708b45966977e2c3f2b0c6ad12872f1e
lalicr19 <lalicr19@hotmail.com>
2016-05-28 09:04:16 -0600

    Primera Version Calculadora

---
+++ Logica.Negocio/RealizarOperacion.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Logica.Negocio
+{
+    public class RealizarOperacion
+    {
+        double Calcular(double operandoUno, double operandoDos,
+                        string operador)
+        {
+            double elResultado = 0.0;
+
+            // invoque al método correspondiente
+            switch (operador)
+            {
+                case "+":
+                    var laReferencia = new Suma();
+                    elResultado = laReferencia.Operar(operandoUno, operandoDos);
+                    break;
+
+/*
+                case "-":
+                    elResultado = Resta.Operar(operandoUno, operandoDos);
+                    break;
+
+                case "*":
+                    elResultado = Multiplicacion.Operar(operandoUno, operandoDos);
+                    break;
+
+                case "/":
+                    elResultado = Division.Operar(operandoUno, operandoDos);
+                    break;
+*/
+            }
+            return (elResultado);
+        }
+    }
+}
